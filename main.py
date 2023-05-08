32858208ec7b545f2416da833a00f60dc4e6361d
yoga-0731 <66167144+yoga-0731@users.noreply.github.com>
2023-05-08 04:49:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+from art import logo
+
+print(logo + "\n\n")
+print("Welcome to the number guessing game!")
+print("I'm thinking of a number between 1 and 100.")
+num = random.randint(1, 100)
+level = input("Choose a diffculty (easy or hard): ")
+
+attempts = 5 if level == 'hard' else 10
+print(f"You have {attempts} attempts remaining to guess the number.")
+game_over = False
+
+while attempts > 0 and not game_over:
+  guess = int(input("\nMake a guess: "))
+  if guess > num:
+    print("Too high")
+  elif guess < num:
+    print("Too low")
+  else:
+    print("You guessed it right!! You WIN!!!")
+    game_over = True
+  if game_over == False:
+    attempts -= 1
+    print(f"Guess again\nYou have {attempts} more to guess again.")
+
+if not game_over:
+  print(f"You've run out of guesses!!\nThe correct number was {num}")
+  
